bd0ac2502bb1d9001095951cade444b24a47a243
SOLENE <>
2017-10-25 03:59:31 +0900

    10월25일04시

---
+++ src/net/nice19/smboard/interceptor/SessionInterceptor.java
@@ -0,0 +1,9 @@
+package net.nice19.smboard.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SessionInterceptor extends HandlerInterceptorAdapter{
+
+
+
+}
